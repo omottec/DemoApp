6de851f576d3c13cd46a866b4af11cf1da38f59d
omotec <omottec@gmail.com>
2017-03-15 01:35:51 +0800

    BaseActivity complete

--- app/src/main/java/com/omottec/demoapp/activity/BaseActivity.java
+++ app/src/main/java/com/omottec/demoapp/activity/BaseActivity.java
@@ -1,11 +1,14 @@
 package com.omottec.demoapp.activity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
 
 import com.omottec.demoapp.R;
@@ -15,70 +18,149 @@ import com.omottec.demoapp.R;
  */
 
 public abstract class BaseActivity extends FragmentActivity {
-    protected static final int LOADING = 0;
-    protected static final int FINISH = 1;
-    protected static final int EMPTY = 2;
-    protected static final int ERROR = 3;
+    protected static final int STATE_LOADING = 0;
+    protected static final int STATE_NORMAL = 1;
+    protected static final int STATE_EMPTY = 2;
+    protected static final int STATE_ERROR = 3;
     private View mRootView;
     private Toolbar mToolbar;
     private FrameLayout mContainer;
     private View mContentView;
     private View mLoadingView;
+    private ValueAnimator mLoadingAnimator;
     private View mErrorView;
     private View mEmptyView;
+    private View.OnClickListener mOnClickEmptyOrErrorListener;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         mRootView = View.inflate(this, R.layout.a_base, null);
         mToolbar = (Toolbar) mRootView.findViewById(R.id.tb);
         mContainer = (FrameLayout) mRootView.findViewById(R.id.container);
+        mContentView = createContentView();
+        mContainer.addView(mContentView);
+        setContentView(mRootView);
     }
 
     protected View createLoadingView() {
-        return null;
+        mLoadingView = View.inflate(this, R.layout.l_loading, null);
+        return mLoadingView;
     }
 
+    protected ValueAnimator createLoadingAnimator() {
+        ValueAnimator loadingAnimator = ObjectAnimator.ofFloat(mLoadingView.findViewById(R.id.iv_loading),"rotation",0,360);
+        loadingAnimator.setDuration(1500);
+        loadingAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        loadingAnimator.setRepeatMode(ValueAnimator.RESTART);
+        loadingAnimator.setInterpolator(new LinearInterpolator());
+        return loadingAnimator;
+    }
+
+
     protected View createErrorView() {
-        return null;
+        mErrorView = View.inflate(this, R.layout.l_net_error, null);
+        if (mOnClickEmptyOrErrorListener != null)
+            mErrorView.setOnClickListener(mOnClickEmptyOrErrorListener);
+        return mErrorView;
     }
 
     protected View createEmptyView() {
-        return null;
+        mEmptyView = View.inflate(this, R.layout.l_empty, null);
+        if (mOnClickEmptyOrErrorListener != null)
+            mEmptyView.setOnClickListener(mOnClickEmptyOrErrorListener);
+        return mEmptyView;
     }
 
-    protected View createContentView() {
-        return null;
+    protected abstract View createContentView();
+
+    protected void setOnClickEmptyOrErrorListener(View.OnClickListener listener) {
+        mOnClickEmptyOrErrorListener = listener;
     }
 
     protected void setState(int state) {
         switch (state) {
-            case LOADING:
+            case STATE_LOADING:
                 showLoading();
                 break;
-            case FINISH:
-                showFinish();
+            case STATE_NORMAL:
+                showNormal();
                 break;
-            case ERROR:
+            case STATE_ERROR:
                 showError();
                 break;
-            case EMPTY:
+            case STATE_EMPTY:
                 showEmpty();
                 break;
         }
     }
 
     private void showEmpty() {
-
+        if (mContentView != null) mContentView.setVisibility(View.GONE);
+        dismissLoading();
+        if (mErrorView != null) mErrorView.setVisibility(View.GONE);
+        if (mEmptyView == null) {
+            mEmptyView = createEmptyView();
+            mContainer.addView(mEmptyView);
+        }
+        mEmptyView.setVisibility(View.VISIBLE);
     }
 
     private void showError() {
+        if (mContentView != null) mContentView.setVisibility(View.GONE);
+        dismissLoading();
+        if (mEmptyView != null) mEmptyView.setVisibility(View.GONE);
+        if (mErrorView == null) {
+            mErrorView = createErrorView();
+            mContainer.addView(mErrorView);
+        }
+        mErrorView.setVisibility(View.VISIBLE);
     }
 
-    private void showFinish() {
+    private void showNormal() {
+        if (mContentView != null) mContentView.setVisibility(View.VISIBLE);
+        dismissLoading();
+        if (mEmptyView != null) mEmptyView.setVisibility(View.GONE);
+        if (mErrorView != null) mErrorView.setVisibility(View.GONE);
     }
 
     private void showLoading() {
+        if (mContentView != null) mContentView.setVisibility(View.VISIBLE);
+        if (mEmptyView != null) mEmptyView.setVisibility(View.GONE);
+        if (mErrorView != null) mErrorView.setVisibility(View.GONE);
+        if (null == mLoadingView) {
+            mLoadingView = createLoadingView();
+            mLoadingView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    return true;
+                }
+            });
+            mContainer.addView(mLoadingView);
+        }
+        mLoadingView.setVisibility(View.VISIBLE);
+        if (mLoadingAnimator == null) mLoadingAnimator = createLoadingAnimator();
+        mLoadingAnimator.start();
+    }
 
+    private void dismissLoading() {
+        if (mLoadingView != null) mLoadingView.setVisibility(View.GONE);
+        if (mLoadingAnimator != null) mLoadingAnimator.cancel();
+    }
+
+    private boolean isLoading() {
+        return mLoadingView != null
+                && mLoadingView.getVisibility() == View.VISIBLE
+                && mLoadingAnimator != null
+                && mLoadingAnimator.isRunning();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isLoading()) {
+            dismissLoading();
+            return;
+        }
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/omottec/demoapp/activity/NetAccessActivity.java
@@ -0,0 +1,83 @@
+package com.omottec.demoapp.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+
+import com.omottec.demoapp.R;
+
+/**
+ * Created by omottec on 14/03/2017.
+ */
+
+public class NetAccessActivity extends BaseActivity implements View.OnClickListener {
+    public static final String TAG = "NetAccessActivity";
+    private Handler mHandler;
+    private TextView mLoadingTv;
+    private TextView mNormalTv;
+    private TextView mEmptyTv;
+    private TextView mErrorTv;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler();
+        mLoadingTv = (TextView) findViewById(R.id.tv_loading);
+        mNormalTv = (TextView) findViewById(R.id.tv_normal);
+        mEmptyTv = (TextView) findViewById(R.id.tv_empty);
+        mErrorTv = (TextView) findViewById(R.id.tv_error);
+        mLoadingTv.setOnClickListener(this);
+        mNormalTv.setOnClickListener(this);
+        mEmptyTv.setOnClickListener(this);
+        mErrorTv.setOnClickListener(this);
+        setState(STATE_LOADING);
+        setOnClickEmptyOrErrorListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setState(STATE_LOADING);
+            }
+        });
+    }
+
+    @Override
+    protected View createContentView() {
+        return View.inflate(this, R.layout.a_net_access, null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setState(STATE_NORMAL);
+            }
+        }, 10 * 1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_loading:
+                setState(STATE_LOADING);
+                break;
+            case R.id.tv_normal:
+                setState(STATE_NORMAL);
+                break;
+            case R.id.tv_empty:
+                setState(STATE_EMPTY);
+                break;
+            case R.id.tv_error:
+                setState(STATE_ERROR);
+                break;
+        }
+    }
+}
