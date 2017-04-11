b171433c6c8479679f9ddcb637987b73d6bdd207
omottec <omottec@gmail.com>
2017-04-11 15:50:28 +0800

    optimize import

--- app/src/main/java/com/omottec/demoapp/view/recycler/OnItemClickListener.java
+++ app/src/main/java/com/omottec/demoapp/view/recycler/OnItemClickListener.java
@@ -1,10 +1,7 @@
 package com.omottec.demoapp.view.recycler;
 
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewParent;
-import android.widget.AdapterView;
 
 /**
  * Created by qinbingbing on 11/04/2017.
--- app/src/main/java/com/omottec/demoapp/view/recycler/OnItemLongClickListener.java
+++ app/src/main/java/com/omottec/demoapp/view/recycler/OnItemLongClickListener.java
@@ -2,7 +2,6 @@ package com.omottec.demoapp.view.recycler;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewParent;
 
 /**
  * Created by qinbingbing on 11/04/2017.
