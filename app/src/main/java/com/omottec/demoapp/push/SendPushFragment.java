fc530647f707e1e0ebc1ae9db611ecfce67d3e3a
omottec <omottec@gmail.com>
2017-06-07 17:23:26 +0800

    optimize import

--- app/src/main/java/com/omottec/demoapp/push/SendPushFragment.java
+++ app/src/main/java/com/omottec/demoapp/push/SendPushFragment.java
@@ -1,15 +1,9 @@
 package com.omottec.demoapp.push;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
