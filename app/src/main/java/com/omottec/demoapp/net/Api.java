5fe897c61aa996e1e9768ae731c8b5bedabd5d40
omottec <omottec@gmail.com>
2017-07-03 11:30:23 +0800

    修改获取网络框架逻辑，加入缓存机制

--- app/src/main/java/com/omottec/demoapp/net/Api.java
+++ app/src/main/java/com/omottec/demoapp/net/Api.java
@@ -2,6 +2,10 @@ package com.omottec.demoapp.net;
 
 import android.support.v4.util.LruCache;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -12,13 +16,33 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public final class Api {
+    private Retrofit mAliyunRetrofit;
+
+    private Map<Class<?>, Retrofit> mApiRetrofitMap = new ConcurrentHashMap<>();
+
+    private LruCache<Class<?>, Object> mApiCache = new LruCache<>(5);
+
+    public <T> T get(Class<T> clazz) {
+        T api = (T) mApiCache.get(clazz);
+        if (api != null) return api;
+        synchronized (mApiCache) {
+            api = (T) mApiCache.get(clazz);
+            if (api == null) {
+                api = mApiRetrofitMap.get(clazz).create(clazz);
+                mApiCache.put(clazz, api);
+            }
+        }
+        return api;
+    }
+
     private Api() {
-        mRetrofit = new Retrofit.Builder()
+        mAliyunRetrofit = new Retrofit.Builder()
                 .baseUrl("http://gc.ditu.aliyun.com/")
 //                .client(new OkHttpClient())
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
+        mApiRetrofitMap.put(IGeoCoding.class, mAliyunRetrofit);
     }
 
     private static final class ApiHolder {
@@ -29,11 +53,4 @@ public final class Api {
         return ApiHolder.INSTANCE;
     }
 
-    private LruCache<Class<?>, Retrofit> mRetrofitLruCache = new LruCache<>(5);
-
-    private Retrofit mRetrofit;
-
-    public <T> T get(Class<T> clazz) {
-        return mRetrofit.create(clazz);
-    }
 }
