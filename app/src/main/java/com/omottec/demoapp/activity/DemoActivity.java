65768813dbbfba7b606383d48316df9c8ae54547
omottec <omottec@gmail.com>
2018-01-22 14:17:39 +0800

    test TextUtils

--- app/src/main/java/com/omottec/demoapp/activity/DemoActivity.java
+++ app/src/main/java/com/omottec/demoapp/activity/DemoActivity.java
@@ -13,6 +13,7 @@ import com.omottec.demoapp.fragment.StateFragment;
 import com.omottec.demoapp.gson.GsonFragment;
 import com.omottec.demoapp.storage.StorageFragment;
 import com.omottec.demoapp.text.TextFragment;
+import com.omottec.demoapp.text.TextUtilsFragment;
 import com.omottec.demoapp.utils.TimeLogger;
 
 import java.sql.Time;
@@ -39,6 +40,6 @@ public class DemoActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new StorageFragment();
+        return new TextUtilsFragment();
     }
 }
---
+++ app/src/main/java/com/omottec/demoapp/text/TextUtilsFragment.java
@@ -0,0 +1,40 @@
+package com.omottec.demoapp.text;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.omottec.demoapp.R;
+import com.omottec.demoapp.Tag;
+
+/**
+ * Created by qinbingbing on 22/01/2018.
+ */
+
+public class TextUtilsFragment extends Fragment {
+    public static final String TAG = "TextUtilsFragment";
+    private TextView mTv;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.full_screen_text, container, false);
+        mTv = view.findViewById(R.id.tv);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mTv.setText("TextUtilsFragment");
+        String str = "";
+        boolean empty = TextUtils.isEmpty(str);
+        boolean digitsOnly = TextUtils.isDigitsOnly(str);
+        Log.d(TAG, "empty:" + empty + ", digitsOnly:" + digitsOnly);
+    }
+}
