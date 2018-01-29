2b41e3f1e8bd82157ab24f35bdb520c4525e302a
omottec <omottec@gmail.com>
2018-01-29 19:46:13 +0800

    show dialog when finishing activity

--- app/src/main/java/com/omottec/demoapp/activity/LauncherActivity.java
+++ app/src/main/java/com/omottec/demoapp/activity/LauncherActivity.java
@@ -1,22 +1,20 @@
 package com.omottec.demoapp.activity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.omottec.demoapp.R;
-import com.omottec.demoapp.Tag;
 import com.omottec.demoapp.utils.Logger;
 
 /**
  * Created by qinbingbing on 9/20/16.
  */
-public class DialogActivity extends FragmentActivity {
-    public static final String TAG = "DialogActivity";
+public class LauncherActivity extends FragmentActivity {
     private TextView mTv;
 
     @Override
@@ -25,20 +23,8 @@ public class DialogActivity extends FragmentActivity {
         setContentView(R.layout.full_screen_text);
         mTv = findViewById(R.id.tv);
         mTv.setOnClickListener(v -> {
-            finish();
-            mTv.postDelayed(() -> {
-                showDialog();
-            }, 3000);
+            Intent intent = new Intent(LauncherActivity.this, DialogActivity.class);
+            startActivity(intent);
         });
-    }
-
-    private void showDialog() {
-        Logger.d(TAG, "showDialog isFinishing:" + isFinishing());
-        Dialog dialog = new Dialog(getApplicationContext());
-        TextView tv = new TextView(this);
-        tv.setText("DialogActivity");
-        dialog.setContentView(tv);
-        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR);
-        dialog.show();
     }
 }
