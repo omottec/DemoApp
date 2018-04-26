bbef0ef43453fbc5433afb197aa582e87d3a20d8
omottec <omottec@gmail.com>
2018-04-26 12:49:26 +0800

    serial access ValueListEmitterLoop

---
+++ app/src/main/java/com/omottec/demoapp/rxjava/serializedAccess/ValueListEmitterLoop.java
@@ -0,0 +1,58 @@
+package com.omottec.demoapp.rxjava.serializedAccess;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by qinbingbing on 26/04/2018.
+ */
+
+public class ValueListEmitterLoop<T> {
+    List<T> queue;
+    boolean emitting;
+    Consumer<? super T> consumer;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void emit(T value) {
+        synchronized (this) {
+            if (emitting) {
+                List<T> q = queue;
+                if (q == null) {
+                    q = new ArrayList<>();
+                    queue = q;
+                }
+                q.add(value);
+                return;
+            }
+            emitting = true;
+        }
+        boolean skipFinal = false;
+        try {
+            consumer.accept(value);
+            for(;;) {
+                List<T> q;
+                synchronized (this) {
+                    q = queue;
+                    if (q == null) {
+                        emitting = false;
+                        skipFinal = true;
+                        return;
+                    }
+                    queue = null;
+                }
+                q.forEach(consumer);
+            }
+        } finally {
+            if (!skipFinal) {
+                synchronized (this) {
+                    emitting = false;
+                }
+            }
+        }
+
+    }
+}
