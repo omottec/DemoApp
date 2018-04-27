98205096e97c79f6b86027614d5659119987b528
omottec <omottec@gmail.com>
2018-04-27 17:53:30 +0800

    no block serailized access ValueQueueDrainFastpath

---
+++ app/src/main/java/com/omottec/demoapp/rxjava/serializedAccess/ValueQueueDrainFastpath.java
@@ -0,0 +1,43 @@
+package com.omottec.demoapp.rxjava.serializedAccess;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+import rx.internal.util.unsafe.MpscLinkedQueue;
+
+/**
+ * Created by qinbingbing on 27/04/2018.
+ */
+
+public class ValueQueueDrainFastpath<T> {
+    final Queue<T> queue = new MpscLinkedQueue<>();
+    final AtomicInteger wip = new AtomicInteger();
+    Consumer consumer;
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void drain(T value) {
+        Objects.requireNonNull(value);
+        if (wip.compareAndSet(0, 1)) {
+            consumer.accept(value);
+            if (wip.decrementAndGet() == 0) {
+                return;
+            }
+        } else {
+            queue.offer(value);
+            if (wip.getAndIncrement() != 0) {
+                return;
+            }
+        }
+        do {
+            T v = queue.poll();
+            consumer.accept(v);
+        } while (wip.decrementAndGet() != 0);
+    }
+}
