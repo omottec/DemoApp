1fcffc4d9e2ceef23c2b2f486529e899c9649152
omotec <omottec@gmail.com>
2018-07-18 22:49:44 +0800

    add Serializable module for lint

---
+++ app/src/main/java/com/omottec/demoapp/serialize/Module.java
@@ -0,0 +1,8 @@
+package com.omottec.demoapp.serialize;
+
+/**
+ * Created by omottec on 18/07/2018.
+ */
+
+public class Module {
+}
---
+++ app/src/main/java/com/omottec/demoapp/serialize/Project.java
@@ -0,0 +1,11 @@
+package com.omottec.demoapp.serialize;
+
+import java.io.Serializable;
+
+/**
+ * Created by omottec on 18/07/2018.
+ */
+
+public class Project implements Serializable {
+    public Module module;
+}
