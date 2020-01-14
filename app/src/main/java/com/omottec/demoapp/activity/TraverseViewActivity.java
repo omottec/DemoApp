d0b59b77982485e6caa653e7801a6ec38bb3e822
omottec <omottec@gmail.com>
2020-01-14 11:04:45 +0800

    log view tag on traverse view

--- app/src/main/java/com/omottec/demoapp/activity/TraverseViewActivity.java
+++ app/src/main/java/com/omottec/demoapp/activity/TraverseViewActivity.java
@@ -18,6 +18,8 @@ public class TraverseViewActivity extends Activity implements View.OnClickListen
     public static final String TAG = "TraverseViewActivity";
     private TextView mTraverseBreadth;
     private TextView mTraverseDepth;
+    private TextView mTraverseBreadthList;
+    private TextView mTraverseDepthList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,20 +27,29 @@ public class TraverseViewActivity extends Activity implements View.OnClickListen
         setContentView(R.layout.a_traverse_view);
         mTraverseBreadth = (TextView) findViewById(R.id.tv_traverse_breadth);
         mTraverseDepth = (TextView) findViewById(R.id.tv_traverse_depth);
+        mTraverseBreadthList = findViewById(R.id.tv_traverse_breadth_list);
+        mTraverseDepthList = findViewById(R.id.tv_traverse_depth_list);
 
         mTraverseBreadth.setOnClickListener(this);
         mTraverseDepth.setOnClickListener(this);
+        mTraverseBreadthList.setOnClickListener(this);
+        mTraverseDepthList.setOnClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        traverseViewDepth(getWindow().getDecorView());
+    }
+
+    private String getViewStr(View view) {
+        if (view.getTag() != null)
+            return view.getTag().toString();
+        return view.toString();
     }
 
     private void traverseViewDepth(View view) {
         if (view == null) return;
-        Logger.i(TAG, view.toString());
+        Logger.i(TAG, getViewStr(view));
 
         if (!(view instanceof ViewGroup)) return;
         ViewGroup group = (ViewGroup) view;
@@ -50,7 +61,7 @@ public class TraverseViewActivity extends Activity implements View.OnClickListen
     private void traverseViewBreadth(View... views) {
         if (views == null || views.length == 0) return;
         for (View view : views)
-            Logger.i(TAG, view.toString());
+            Logger.i(TAG, getViewStr(view));
         Logger.i(TAG, "======================");
         List<View> list = new ArrayList<>();
         for (View view : views) {
@@ -68,7 +79,7 @@ public class TraverseViewActivity extends Activity implements View.OnClickListen
         list.offerLast(root);
         while (list.size() > 0) {
             View view = list.pollFirst();
-            Logger.i(TAG, view.toString());
+            Logger.i(TAG, getViewStr(view));
             if (view instanceof ViewGroup) {
                 ViewGroup group = (ViewGroup) view;
                 for (int i = 0; i < group.getChildCount(); i++)
@@ -83,7 +94,7 @@ public class TraverseViewActivity extends Activity implements View.OnClickListen
         list.offerLast(root);
         while (list.size() > 0) {
             View view = list.pollLast();
-            Logger.i(TAG, view.toString());
+            Logger.i(TAG, getViewStr(view));
             if (view instanceof ViewGroup) {
                 ViewGroup group = (ViewGroup) view;
                 for (int i = 0; i < group.getChildCount(); i++)
@@ -96,12 +107,16 @@ public class TraverseViewActivity extends Activity implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_traverse_breadth:
-                traverseBreadthByList(getWindow().getDecorView());
-//                traverseViewBreadth(getWindow().getDecorView());
+                traverseViewBreadth(getWindow().getDecorView());
                 break;
             case R.id.tv_traverse_depth:
+                traverseViewDepth(getWindow().getDecorView());
+                break;
+            case R.id.tv_traverse_breadth_list:
+                traverseBreadthByList(getWindow().getDecorView());
+                break;
+            case R.id.tv_traverse_depth_list:
                 traverseDepthByList(getWindow().getDecorView());
-//                traverseViewDepth(getWindow().getDecorView());
                 break;
         }
     }
