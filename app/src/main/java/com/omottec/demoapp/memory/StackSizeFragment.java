7f91de31b0d0891cdf2c2c37a9c3e63e9fa2e4f5
omottec <omottec@gmail.com>
2020-10-16 17:16:46 +0800

    set env variable

--- app/src/main/java/com/omottec/demoapp/memory/StackSizeFragment.java
+++ app/src/main/java/com/omottec/demoapp/memory/StackSizeFragment.java
@@ -1,15 +1,21 @@
 package com.omottec.demoapp.memory;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.system.ErrnoException;
+import android.system.Os;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import com.omottec.demoapp.R;
+import java.util.Map;
 
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class StackSizeFragment extends Fragment {
     public static final String TAG = "StackSizeFragment";
     @Nullable
@@ -39,7 +45,8 @@ public class StackSizeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 //defaultStackSizeThread.start();
-                optStackSizeThread.start();
+                //optStackSizeThread.start();
+                sysEnv();
             }
         });
     }
@@ -48,5 +55,20 @@ public class StackSizeFragment extends Fragment {
         Log.i(TAG, "recursive " + name + i);
         i++;
         recursive(name, i);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void sysEnv() {
+        Map<String, String> env = System.getenv();
+        Log.i(TAG, "System.getenv:" + env.toString());
+        String rustMinStack = Os.getenv("RUST_MIN_STACK");
+        Log.i(TAG, "rustMinStack:" + rustMinStack);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                Os.setenv("RUST_MIN_STACK", "1048576", true);
+            } catch (ErrnoException e) {
+                Log.e(TAG, "", e);
+            }
+        }
     }
 }
