6d1a960ae110dfbe94c52d1556fb2903c4f91d06
omottec <omottec@gmail.com>
2021-04-23 12:05:39 +0800

    add Dependencies.java

---
+++ buildSrc/src/main/java/com/omottec/demoapp/Dependencies.java
@@ -0,0 +1,100 @@
+package com.omottec.demoapp;
+
+public final class Dependencies {
+
+    public static final int MIN_SDK_VERSION = 21;
+    public static final int MIN_SDK_VERSION_1 = 21;
+    public static final int MIN_SDK_VERSION_2 = 21;
+    public static final int MIN_SDK_VERSION_3 = 21;
+    public static final int MIN_SDK_VERSION_4 = 21;
+    public static final int TARGET_SDK_VERSION = 25;
+    public static final int COMPILE_SDK_VERSION = 29;
+    public static final String BUILD_TOOLS_VERSION = "29.0.2";
+    public static final String SUPPORT_LIBRARY_VERSION = "27.1.1";
+
+    public static final String ANDROID_APP_PLUGIN = "com.android.application";
+    public static final String ANDROID_LIB_PLUGIN = "com.android.library";
+
+    public static final String JAVA_LIB_PLUGIN = "java-library";
+    public static final String JAVA_PLUGIN = "java";
+    public static final String GROOVY_PLUGIN = "groovy";
+    public static final String MAVEN_PLUGIN = "maven";
+    public static final String MAVEN_PUBLISH_PLUGIN = "maven-publish";
+
+    public static final String KOTLIN_CLASSPATH = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21";
+    public static final String KOTLIN_PLUGIN_ID = "kotlin-android";
+    public static final String KOTLIN_AAR = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.21";
+
+    public static final String GREENDAO_CLASSPATH = "org.greenrobot:greendao-gradle-plugin:3.3.0";
+    public static final String GREENDAO_PLUGIN = "org.greenrobot.greendao";
+    public static final String GREENDAO_AAR = "org.greenrobot:greendao:3.3.0";
+
+    public static final String ASPECTJ_CLASSPATH = "com.hujiang.aspectjx:gradle-android-plugin-aspectjx:2.0.10";
+    public static final String ASPECTJ_PLUGIN = "android-aspectjx";
+    public static final String ASPECTJ_AAR = "org.aspectj:aspectjrt:1.9.5";
+
+    // classpath "group:module:version"
+    public static final String LIFECYCLE_CLASSPATH = "com.omottec.plugin.lifecycle:gradle_plugin_lifecycle:1.0.30";
+    public static final String LIFECYCLE_PLUGIN = "com.omottec.plugin.lifecycle";
+
+    public static final String GRADLE = "com.android.tools.build:gradle:3.6.1";
+
+    public static final String BINTRAY_CLASSPATH = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.+";
+    public static final String BINTRAY_PLUGIN = "com.jfrog.bintray";
+
+    public static final String ASM_CLASSPATH = "com.omottec.plugin.asm:gradle_plugin_asm:2.0.3";
+    public static final String ASM_PLUGIN = "com.omottec.plugin.asm";
+    public static final String ASM_AAR = "org.ow2.asm:asm:9.0";
+    public static final String ASM_COMMON_AAR = "org.ow2.asm:asm-commons:9.0";
+
+    public static final String LOGGER = "com.github.omottec:logger:0.1.0";
+
+    public static final String MULTIDEX = "androidx.multidex:multidex:2.0.0";
+    public static final String APPCOMPAT = "androidx.appcompat:appcompat:1.0.0";
+    public static final String CARDVIEW = "androidx.cardview:cardview:1.0.0";
+    public static final String LEGACY_SUPPORT_V4 = "androidx.legacy:legacy-support-v4:1.0.0";
+    public static final String ANNOTATION = "androidx.annotation:annotation:1.0.0";
+
+    public static final String MATERIAL = "com.google.android.material:material:1.0.0";
+    public static final String GSON = "com.google.code.gson:gson:2.8.0";
+
+    public static final String MCXIAOKE_VOLLEY = "com.mcxiaoke.volley:library:1.0.18";
+    public static final String ANDROID_VOLLEY = "com.android.volley:volley:1.0.0";
+
+    public static final String OKHTTP3 = "com.squareup.okhttp3:okhttp:3.12.0";
+
+    public static final String RETROFIT2_RETROFIT = "com.squareup.retrofit2:retrofit:2.3.0";
+    public static final String RETROFIT2_CONVERTER = "com.squareup.retrofit2:converter-gson:2.3.0";
+    public static final String RETROFIT2_ADAPTER = "com.squareup.retrofit2:adapter-rxjava:2.3.0";
+
+    public static final String FRESCO_FRESCO = "com.facebook.fresco:fresco:1.2.0";
+    public static final String FRESCO_IMAGEPIPELINE = "com.facebook.fresco:imagepipeline-okhttp:0.5.0";
+
+    public static final String GLIDE_AAR = "com.github.bumptech.glide:glide:4.11.0";
+    public static final String GLIDE_ANNOTATION_PROCESSOR = "com.github.bumptech.glide:compiler:4.11.0";
+
+    public static final String RXJAVA2 = "io.reactivex.rxjava2:rxjava:2.0.7";
+    public static final String RXLIFECYCLE2 = "com.trello.rxlifecycle2:rxlifecycle:2.0.1";
+    public static final String RXLIFECYCLE2_ANDROID = "com.trello.rxlifecycle2:rxlifecycle-android:2.1.0";
+    public static final String RXLIFECYCLE2_ANDROID_LIFECYCLE = "com.trello.rxlifecycle2:rxlifecycle-android-lifecycle:2.1.0";
+    public static final String RXLIFECYCLE2_COMPONENTS = "com.trello.rxlifecycle2:rxlifecycle-components:2.1.0";
+
+    public static final String RXJAVA = "io.reactivex:rxjava:1.2.9";
+    public static final String RXANDROID = "io.reactivex:rxandroid:1.2.1";
+    public static final String RXLIFECYCLE = "com.trello:rxlifecycle:0.3.1";
+    public static final String RXLIFECYCLE_COMPONENTS = "com.trello:rxlifecycle-components:0.3.1";
+    public static final String RXPERMISSIONS = "com.tbruyelle.rxpermissions:rxpermissions:0.9.4@aar";
+
+
+    public static final String LEAKCANARY_ANDROID = "com.squareup.leakcanary:leakcanary-android:1.5";
+    public static final String LEAKCANARY_ANDROID_NO_OP = "com.squareup.leakcanary:leakcanary-android-no-op:1.5";
+
+    public static final String APPSEE_PLUGIN = "mtj-circle-plugin";
+    public static final String APPSEE_AAR = "com.appsee:appsee-android:+";
+
+    public static final String AUTO_SERVICE = "com.google.auto.service:auto-service:1.0-rc4";
+
+    public static final String EPIC = "me.weishu:epic:0.11.0";
+
+    public static final String JAVAPOET = "com.squareup:javapoet:1.12.0";
+}
