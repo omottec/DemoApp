4637d51728b5615b0ac2c7ec019ec2b0b0794c4a
omottec <omottec@gmail.com>
2021-04-23 19:39:26 +0800

    upload logger 0.1.2 to mavenCentral

--- logger/src/main/java/com/omottec/logger/Logger.java
+++ logger/src/main/java/com/omottec/logger/Logger.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
  * Created by qinbingbing on 9/8/16.
  */
 public final class Logger {
+    public static final String VERSION = "0.1.2";
+
     private static int sLevel = Log.VERBOSE;
 
     private static boolean sAppendCallInfo = false;
