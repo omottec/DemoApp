95dc17429839ab7299b8922c96dbe6f46409c073
omottec <omottec@gmail.com>
2021-04-25 14:45:11 +0800

    upload logger 0.1.5

--- logger/src/main/java/com/omottec/logger/Logger.java
+++ logger/src/main/java/com/omottec/logger/Logger.java
@@ -9,7 +9,7 @@ import android.view.LayoutInflater;
  * Created by qinbingbing on 9/8/16.
  */
 public final class Logger {
-    public static final String VERSION = "0.1.4";
+    public static final String VERSION = "0.1.5";
 
     private static int sLevel = Log.VERBOSE;
 
