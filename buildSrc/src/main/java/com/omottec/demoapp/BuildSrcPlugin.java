6d4f5dadcaf0b2157e9a70685dae8d7fc2074b30
omotec <omottec@gmail.com>
2021-06-04 20:36:30 +0800

    remove unnecessary dependency of gradle api

--- buildSrc/src/main/java/com/omottec/demoapp/BuildSrcPlugin.java
+++ buildSrc/src/main/java/com/omottec/demoapp/BuildSrcPlugin.java
@@ -2,11 +2,10 @@ package com.omottec.demoapp;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.jetbrains.annotations.NotNull;
 
 public class BuildSrcPlugin implements Plugin<Project> {
     @Override
-    public void apply(@NotNull Project target) {
+    public void apply(Project target) {
         System.out.println("BuildSrcPlugin apply " + target);
     }
 }
