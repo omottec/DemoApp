29bd1b98a14c879c691f2b67358ab0ab0bc85edc
omottec <omottec@gmail.com>
2021-06-07 18:09:08 +0800

    log ClassLoader

--- buildSrc/src/main/java/BuildSrcPlugin.java
+++ buildSrc/src/main/java/BuildSrcPlugin.java
@@ -5,5 +5,7 @@ public class BuildSrcPlugin implements Plugin<Project> {
     @Override
     public void apply(Project target) {
         System.out.println("BuildSrcPlugin apply " + target);
+        ClassLoader classLoader = target.getClass().getClassLoader();
+        System.out.println("ClassLoader:" + classLoader + ", hashCode:" + classLoader.hashCode());
     }
 }
